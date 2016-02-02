68b46cf0f4565f05b642821c19763f053c104b8f
Eugene Sharygin <eush77@gmail.com>
2016-02-03 02:37:03 +0300

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+[![npm](https://nodei.co/npm/module-usage-viewer.png)](https://npmjs.com/package/module-usage-viewer)
+
+# module-usage-viewer
+
+[![Dependency Status][david-badge]][david]
+
+Open examples of npm module usage in editor.
+
+[david]: https://david-dm.org/eush77/module-usage-viewer
+[david-badge]: https://david-dm.org/eush77/module-usage-viewer.png
+
+## Install
+
+```
+npm install module-usage-viewer
+```
+
+## License
+
+MIT
---
+++ cli.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+'use strict';
+
+var moduleUsage = require('module-usage'),
+    npmGet = require('npm-get'),
+    editor = require('string-editor'),
+    Queue = require('push-queue'),
+    App = require('help-version');
+
+var path = require('path');
+
+
+var app = App('Usage:  module-usage-viewer <name>');
+
+(function main (argv) {
+  if (argv.length != 1) {
+    return app.help(1);
+  }
+
+  var enqueue = Queue(function (usage, next) {
+    console.log('Opening %s...', usage.dependant);
+
+    npmGet(usage.dependant, usage.file, function (err, _, content) {
+      if (err) throw err;
+
+      var displayName = path.join(usage.dependant, usage.file)
+            .replace(RegExp(path.sep, 'g'), '|');
+
+      editor(content, displayName, function (err) {
+        if (err) throw err;
+        next();
+      });
+    });
+  });
+
+  moduleUsage(argv[0]).on('data', enqueue);
+}(process.argv.slice(2)));
